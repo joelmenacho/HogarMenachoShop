0c4d79d9f210591bb4142b2bff95edfa7fb24405
unknown <joelmenacho@gmail.com>
2022-09-20 12:13:20 -0500

    mi primer cambio

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        href="#"
+                        className="repo_link"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        HOGAR MENACHO
+                    </a>
+
+                    <p>
+                        Desing by | &nbsp;
+                        <a
+                            href="https://www.linkedin.com/in/joelmenachoa/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Joel Menacho
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
---
+++ src/data/productsData.js
@@ -0,0 +1,68 @@
+const productsData = [
+    {
+        id: 1,
+        img: "/images/prod1.png",
+        rating: "★★★★",
+        title: "Hero GoPro 10",
+        price: 989,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "/images/prod2.png",
+        rating: "★★★★★",
+        title: "Canon 1500D",
+        price: 3100,
+        quantity: 2,
+    },
+    {
+        id: 3,
+        img: "/images/prod3.png",
+        rating: "★★★★",
+        title: "Nikon Z 50",
+        price: 2999,
+        quantity: 10,
+    },
+    {
+        id: 4,
+        img: "/images/prod4.png",
+        rating: "★★★★★",
+        title: "Sony RX100",
+        price: 4999,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "/images/prod5.png",
+        rating: "★★★★",
+        title: "Sony Lens",
+        price: 3999,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "/images/prod6.png",
+        rating: "★★★★★",
+        title: "Hero GoPro 9",
+        price: 1999,
+        quantity: 1,
+    },
+    {
+        id: 7,
+        img: "/images/prod7.png",
+        rating: "★★★★",
+        title: "Canon Flash",
+        price: 2499,
+        quantity: 1,
+    },
+    {
+        id: 8,
+        img: "/images/prod8.png",
+        rating: "★★★★",
+        title: "Canon EOS",
+        price: 2999,
+        quantity: 1,
+    }
+];
+
+export default productsData;
